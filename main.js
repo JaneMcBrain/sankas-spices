172c83bdd23b7465d8bb81f3936adf95a7b4169b
Janine Semmler <syries@gmx.de>
2021-03-23 18:19:39 +0100

    update content, restructure sass files

--- main.js
+++ main.js
@@ -1,47 +1,19 @@
-'use-strict';
+'use strict'
 
-const products = [
-  {name: 'Chilli powder', price: 0.65},
-  {name: 'Chilli pieces', price:0.65},
-  {name: 'Curry powder', price:0.6},
-  {name: 'Meat Curry powder', price:1.00},
-  {name: 'Roasted Curry powder', price:0.90},
-  {name: 'Mustard powder', price:0.50},
-  {name: 'Pepper powder', price:1.10},
-  {name: 'Peppercorn', price:1.95},
-  {name: 'Yellow Turmeric', price:3.45},
-  {name: 'Ginger', price:0.90},
-  {name: 'Ginger powder', price:0.65},
-  {name: 'Fenugreek', price:0.90},
-  {name: 'Cinnamon', price:1.30},
-  {name: 'Cardomon', price:1.95},
-  {name: 'Masala', price:1.10}
-]
+const activeClass ="is-active";
 
-const createTable = () => {
-  const headRows = ['Produkte', 'Euro pro 100g'];
-  headRows.map(appendRow);
+const popups = ['story', 'withdrawal', 'paypal', 'impressum'];
+
+const initPopup = (type) => {
+  const open = document.querySelector(`.js-open-${type}`);
+  const close = document.querySelector(`.js-${type}-close`);
+  const popup = document.querySelector(`.js-${type}-popup`)
+  open.addEventListener('click', () => {
+    popup.classList.add(activeClass);
+  });
+  close.addEventListener('click', () => {
+    popup.classList.remove(activeClass);
+  });
 }
 
-const appendRow = (item, i) => document.querySelector('.spice-table').appendChild(createRow(item, i));
-const appendCell = (row, type, text) => row.appendChild(generateCell(type, text));
-
-const generateCell = (type, text) => {
-  const cell = document.createElement('p');
-  cell.className = `spice-table__${type}`
-  cell.textContent = text;
-  return cell;
-}
-
-const createRow = (text, index) => {
-  const row = document.createElement('span');
-  row.className = 'spice-table__row'
-  appendCell(row, 'head', text);
-  if(index === 0) products.map(item => appendCell(row, 'cell', item.name));
-  if(index === 1) products.map(item => appendCell(row, 'cell', convertToEuro(item.price)));
-  return row;
-}
-
-const convertToEuro = num => `${num.toFixed(2)} Euro`;
-
-createTable();
+popups.forEach(type => initPopup(type));
